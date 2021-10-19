8e3880075a848e6c2b4824b095c533e7d31f258a
fengyf <fengyf2@shanghaitech.edu.cn>
2021-10-19 12:12:34 +0000

    comments of fixed_point.h

--- src/threads/fix_point.h
+++ src/threads/fix_point.h
@@ -35,29 +35,41 @@
 /* Type for fix point numbers. */
 typedef int fix_point;
 
+/* Shifting q */
 #define FIX_POINT_SHIFT 16
 
+/* Convert int x to fixed point. */
 #define int_to_fix(x) ((fix_point)(x << FIX_POINT_SHIFT))
 
+/* Convert x to integer (rounding toward zero). */
 #define fix_to_int_float(x) (x >> FIX_POINT_SHIFT)
 
+/* Convert x to integer (rounding to nearest). */
 #define fix_to_int_round(x) (x >= 0 ? ((x + (1 << (FIX_POINT_SHIFT - 1))) >> FIX_POINT_SHIFT) \
         : ((x - (1 << (FIX_POINT_SHIFT - 1))) >> FIX_POINT_SHIFT))
 
+/* Add x and y. */
 #define add(x,y) (x + y)
 
+/* Add x and n. */
 #define add_int(x,n) (x + (n << FIX_POINT_SHIFT))
 
+/* Subtract y from x */
 #define sub(x,y) (x - y)
 
+/* Subtract n from x */
 #define sub_int(x,n) (x - (n << FIX_POINT_SHIFT))
 
+/* Multiply x by n */
 #define mul_int(x,n) (x * n)
 
+/* Divide x by n */
 #define div_int(x,n) (x / n)
 
+/* Multiply x by y */
 #define mul(x,y) ((fix_point)(((int64_t) x) * y >> FIX_POINT_SHIFT))
 
+/* Divide x by y */
 #define div(x,y) ((fix_point)((((int64_t) x) << FIX_POINT_SHIFT) / y))
 
 #endif 
