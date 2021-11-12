a6f0f4d46a4bb9e08c05090a447dfdd7a32682a0
yangbin <yangbin@shanghaitech.edu.cn>
2021-11-12 14:04:29 +0000

    prev vision

--- src/filesys/file.h
+++ src/filesys/file.h
@@ -6,14 +6,7 @@
 
 struct inode;
 
-/* An open file. */
-struct file 
-  {
-    struct inode *inode;        /* File's inode. */
-    off_t pos;                  /* Current position. */
-    int deny_write;            /* Has file_deny_write() been called? */
 
-  };
 
 /* Opening and closing files. */
 struct file *file_open (struct inode *);
