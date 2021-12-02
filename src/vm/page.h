496b1ea4fcf05b258484516480756fc6698865a0
yangbin <yangbin@shanghaitech.edu.cn>
2021-12-02 06:42:24 +0000

    struct of page

--- src/vm/page.h
+++ src/vm/page.h
@@ -5,12 +5,27 @@
 
 
 struct sup_page_table_entry {
-    uint32_t** pagedir;
+    struct thread* owner;
+    
     
     uint32_t* vadd;
 
-    
+    struct lock page_lock;
+    struct list_elem elem;
 };
 
+struct sup_page_table
+{
+    struct list table;
+    struct lock table_lock;
+};
+
+
+struct sup_page_table_entry sup_page_table_look_up (struct sup_page_table table, void *vaddr);
+struct sup_page_table * sup_page_table_init();
+void sup_page_table_destroy(struct sup_page_table *table);
+void sup_page_create (void *upage, enum palloc_flags flag);
+void sup_page_destroy(struct sup_page_table *table, void *vadd);
+bool sup_page_activate(struct sup_page_table_entry *entry);
 
 #endif /* vm/page.h */
