c0ee938437031522f0f1792e702ecf427f41f91f
yangbin <yangbin@shanghaitech.edu.cn>
2021-12-03 07:29:34 +0000

    start page fault

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -4,6 +4,7 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -147,6 +148,17 @@ page_fault (struct intr_frame *f)
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
+
+   if (!user)
+   {
+      f->eip = f->eax;
+      f->eax = 0xffffffff;
+      exit(-1);
+      return;
+   }
+
+   if (!is_user_vaddr(fault_addr))
+
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
